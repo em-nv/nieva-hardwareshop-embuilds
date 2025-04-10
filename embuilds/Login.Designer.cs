75d630f4ab7fede190ab60c22c1df1d3824a2a74
Emmanuel Nieva <emorielnieva11@gmail.com>
2025-04-10 20:00:34 +0800

    add

--- embuilds/Login.Designer.cs
+++ embuilds/Login.Designer.cs
@@ -128,9 +128,9 @@
             this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(39, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(299, 17);
+            this.label2.Size = new System.Drawing.Size(303, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Pleaseenter your credentials to access the system.";
+            this.label2.Text = "Please enter your credentials to access the system.";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
@@ -138,7 +138,7 @@
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(118, 49);
+            this.label1.Location = new System.Drawing.Point(118, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(143, 25);
             this.label1.TabIndex = 0;
